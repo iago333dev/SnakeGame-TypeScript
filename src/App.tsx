bc6fa9afe4b41db33379d7caeb94080679449869
Iago_Alves333 <45635356+iago333dev@users.noreply.github.com>
2021-02-12 15:42:40 -0300

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,47 @@
 import Board from "./components/Board";
-import React from "react";
+import React, { useState } from "react";
 
-const initialState = {
+interface InitialState {
+  directionX: number;
+  directionY: number;
+  food: number[];
+  size: {
+    x: number;
+    y: number;
+  };
+  snake: number[][];
+  velocity: number;
+  score: number;
+}
+const initialState: InitialState = {
   directionX: 1,
   directionY: 0,
   food: [5, 5],
   size: { x: 60, y: 28 },
-  snake: [ [2, 0] ,[1,0], [0,0]],
+  snake: [[2, 0], [1, 0], [0, 0]],
   velocity: 400,
-  score: 0
+  score: 0,
+  
 };
 
-function getBoard() {
+/*
+
+const updateSnake = () => {
+  const currentHead = initialState.snake[0];
+  const newHead = getNewHead(currentHead);
+  //const board = getBoard();
+  const newHeadState = board[newHead[1]][newHead[0]];
+  
+  console.log(newHead);
+}
+
+
+const getNewHead = (head:number) => {
+  return [head[0] + initialState.directionX, head[1] + initialState.directionY];
+};
+
+*/
+function getBoard({ snake, food }: InitialState) {
 
   const board = [...Array(initialState.size.y)].map(() => {
     return [...Array(initialState.size.x)].map(() => {
@@ -19,16 +49,23 @@ function getBoard() {
     });
   });
 
+  board[food[1]][food[0]] = "FOOD";
+
+  snake.forEach(coords => {
+    board[coords[1]][coords[0]] = "SNAKE";
+  });
+
   return board;
 }
 
 const App: React.FC = () => {
-  const board = getBoard();
+  //const [board, setBoard] = useState()
+  const board = getBoard(initialState);
+  
   return (
-      <Board boardelements={board}>        
-      </Board>
+    <Board boardelements={board}>
+    </Board>
   );
 };
 
 export default App;
-  
